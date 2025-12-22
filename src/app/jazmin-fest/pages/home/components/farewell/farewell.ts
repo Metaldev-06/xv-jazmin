a690d1c7c45d4cab331e755853b136674c6d4c88
Fernando Diaz <fernydiaz62@gmail.com>
2025-12-21 21:27:11 -0300

    feat: add farewell component with template, styles, and tests

---
+++ src/app/jazmin-fest/pages/home/components/farewell/farewell.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-farewell',
+  imports: [],
+  templateUrl: './farewell.html',
+  styleUrl: './farewell.css',
+})
+export class Farewell {
+
+}
--- src/app/jazmin-fest/pages/home/home.ts
+++ src/app/jazmin-fest/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 
 import { Address } from './components/address/address';
 import { Countdown } from './components/countdown/countdown';
+import { Farewell } from './components/farewell/farewell';
 import { GiftComponent } from './components/gift/gift';
 import { Hero } from './components/hero/hero';
 import { ImagesGrid } from './components/images-grid/images-grid';
@@ -10,7 +11,7 @@ import { StarEffect } from '../../../shared/components/star-effect/star-effect';
 
 @Component({
   selector: 'app-home',
-  imports: [StarEffect, Hero, Countdown, Address, GiftComponent, Invitation, ImagesGrid],
+  imports: [StarEffect, Hero, Countdown, Address, GiftComponent, Invitation, ImagesGrid, Farewell],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
