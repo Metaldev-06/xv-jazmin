9e45d4d3914fffda7524d8f4e9c535dcb0911267
Fernando Diaz <fernydiaz62@gmail.com>
2025-12-23 19:27:52 -0300

    feat: update API URL for playlist data submission

--- src/app/core/services/playlist-data.ts
+++ src/app/core/services/playlist-data.ts
@@ -9,7 +9,7 @@ import { PlaylistDataBody, PlaylistDataResponse } from '../interfaces/playlist-d
 export class PlaylistData {
   private readonly http = inject(HttpClient);
   private readonly apiUrl =
-    'https://prueba.metaldev.cloud/webhook/02c667a2-457e-47d7-b51f-5a4d52a83e89';
+    'https://prueba.metaldev.cloud/webhook/4be57ad5-3b46-413f-b018-1150898f8182';
 
   public sendPlaylist(data: PlaylistDataBody): Observable<PlaylistDataResponse> {
     return this.http.post<PlaylistDataResponse>(this.apiUrl, data);
